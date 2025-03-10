c5ee82eb145289bfb961496fd9ef3e133d7d18d6
Mark Luiz <markbobis173@gmail.com>
2025-03-10 16:44:19 +0800

    fixed color not completing

--- screens/BasicColors.js
+++ screens/BasicColors.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import { StyleSheet, View, FlatList } from "react-native";
 import React, { useContext } from "react";
 
 import MainContainer from "../components/MainContainer";
