09fc87f8245fe863a9bad6810631c54c4c71754a
Mark Luiz <markbobis173@gmail.com>
2025-03-13 15:25:31 +0800

    purple only level container

--- components/LevelContainer.js
+++ components/LevelContainer.js
@@ -11,7 +11,7 @@ const LevelContainer = ({
   navigateToGame,
   isComplete,
   height = 120,
-  index, // Pass index when rendering
+  index,
 }) => {
   const { fontSize, buttonSize } = useUserPreferences();
 
@@ -20,40 +20,6 @@ const LevelContainer = ({
 
   // Select ButtonBox based on the index for true alternation
   const ButtonBoxComponent = buttonBoxes[index % buttonBoxes.length];
-
-  const brightColors = useMemo(() => {
-    const colors = [
-      { color: "#FF3B3B", text: "white" },
-      { color: "#FF6347", text: "white" },
-      { color: "#FF4500", text: "white" },
-      { color: "#FFA500", text: "black" },
-      { color: "#FFD700", text: "black" },
-      { color: "#FF69B4", text: "white" },
-      { color: "#FF1493", text: "white" },
-      { color: "#FF00FF", text: "white" },
-      { color: "#DA70D6", text: "white" },
-      { color: "#9932CC", text: "white" },
-      { color: "#8A2BE2", text: "white" },
-      { color: "#4B0082", text: "white" },
-      { color: "#00BFFF", text: "black" },
-      { color: "#1E90FF", text: "white" },
-      { color: "#00CED1", text: "black" },
-      { color: "#20B2AA", text: "black" },
-      { color: "#32CD32", text: "black" },
-      { color: "#00FA9A", text: "black" },
-      { color: "#ADFF2F", text: "black" },
-      { color: "#7FFF00", text: "black" },
-      { color: "#FF7F50", text: "white" },
-      { color: "#DC143C", text: "white" },
-      { color: "#FF8C00", text: "black" },
-      { color: "#F08080", text: "black" },
-      { color: "#00FF7F", text: "black" },
-      { color: "#40E0D0", text: "black" },
-    ];
-
-    // Fisher-Yates Shuffle Algorithm
-    return colors.sort(() => Math.random() - 0.5);
-  }, []);
 
   return (
     <TouchableOpacity
@@ -74,8 +40,8 @@ const LevelContainer = ({
         />
       )}
       <ButtonBoxComponent
-        color={brightColors[index].color}
-        textColor={brightColors[index].text}
+        color={"#7F00FF"}
+        textColor={"#ffffff"}
         text={text}
         fontSize={fontSize * 4}
       />
